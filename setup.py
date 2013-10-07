f0f32cbe450d429bc3534ed11981d3c58f448057
Alex Hartsell <kalhartt@gmail.com>
2013-10-06 20:54:45 -0400

    dependencies added

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
 from setuptools import setup
+import sys
+
+if sys.version_info < (2, 7, 0):
+    install_requires_extras = ['importlib']
+else:
+    install_requires_extras = []
 
 setup(
-        name = "pycclone",
-        version = "0.1.0",
-        description = """An extensible rewrite of Pycco, the
-        python port of Docco: the original quick-and-dirty hundred-line-long,
-        literate-programming-style documentation generator.
-        """,
-        author = "Kalhartt",
-        author_email = "kalhartt@gmail.com",
-        url = "http://kalhartt.github.com/pycclone",
-        packages = ["pycclone", "pycclone.templates", "pycclone.formatters", "pycclone.highlighters"],
-        packages_dir = {
-            "pycclone": "pycclone",
-            "pycclone.templates": "pycclone/templates",
-            "pycclone.formatters": "pycclone/formatters",
-            "pycclone.highlighters": "pycclone/highlighters"
-        },
-        packages_data = {
-            "pycclone": "data/*",
-            "pycclone.templates": "data/*"
-        },
-        install_requires = [],
-        extras_requires = {},
+    name="pycclone",
+    version="0.1.0",
+    description="""An extensible rewrite of Pycco, the
+    python port of Docco: the original quick-and-dirty hundred-line-long,
+    literate-programming-style documentation generator.
+    """,
+    author="Kalhartt",
+    author_email="kalhartt@gmail.com",
+    url="http://kalhartt.github.com/pycclone",
+    packages=["pycclone", "pycclone.templates", "pycclone.formatters", "pycclone.highlighters"],
+    packages_dir={
+        "pycclone": "pycclone",
+        "pycclone.templates": "pycclone/templates",
+        "pycclone.formatters": "pycclone/formatters",
+        "pycclone.highlighters": "pycclone/highlighters"
+    },
+    packages_data={
+        "pycclone": "data/*",
+        "pycclone.templates": "data/*"
+    },
+    install_requires=['markdown', 'pygments'] + install_requires_extras,
+    extras_requires={},
 )
